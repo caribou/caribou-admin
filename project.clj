2d66bf76f0d25717313a5bae6470ac96678e3d58
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-03-11 14:52:42 -0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-admin "0.9.1"
+(defproject antler/caribou-admin "0.9.2"
   :description "Generic admin tool for Caribou projects"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [antler/caribou-frontend "0.9.8"]
