6e3ce237b810d0669b67c1fc1467e316d548ccd2
Justin Glenn Smith <noisesmith@gmail.com>
2013-05-13 12:23:53 -0700

    routes and roles

--- src/caribou/admin/controllers/settings/account.clj
+++ src/caribou/admin/controllers/settings/account.clj
@@ -1,9 +1,13 @@
 (ns caribou.admin.controllers.settings.account
   (:use caribou.app.controller
-        [cheshire.core :only (generate-string)])
-  (:require [caribou.model :as model]))
-  
+        [cheshire.core :only (generate-string)]
+        [caribou.app.pages :only [route-for]])
+  (:require [caribou
+             [model :as model]
+             [auth :as auth]]
+            [caribou.admin.rights :as rights]))
 
+(defn nothing (constantly nil))
 
 (defn index
   [request]
@@ -21,10 +25,6 @@
   [request]
   (render request))
 
-(defn create
-  [request]
-  (render request))
-
 (defn update
   [request]
   (render request))
@@ -33,6 +33,97 @@
   [request]
   (render request))
 
-(defn new
+(defn login
   [request]
   (render request))
+
+(defn submit-login
+  [request]
+  (let [email (-> request :params :email)
+        password (-> request :params :password)
+        locale (or (-> request :params :locale) "global")
+        target (or (-> request :params :target)
+                   (route-for :admin.models {:locale locale :site "admin"}))
+        account (model/pick :account {:where {:email email}})
+        match? (and (seq password)
+                    (seq (:crypted-password account))
+                    (auth/check-password password (:crypted-password account)))
+        target (if-not match?
+                 (str (route-for :admin.login {:locale locale :site "admin"})
+                      "&target=" target)
+                 target)
+        login (if match? "success" "failure")
+        session (:session request)
+        session (if-not match?
+                  session
+                  (assoc session
+                    :admin
+                    {:user (dissoc account :created-at :updated-at)
+                     :locale locale}))]
+    (println "USER in submit-login is " (-> session :admin :user))
+    (redirect target {:session session :login login})))
+
+(defn new
+  [request]
+  (rights/with-permissions "settings/account/new" request
+    (fn [[role-id perms :as permissions] request]
+      (assert (rights/has-perms :account perms [:create] role-id))
+      (let [roles (rights/gather permissions :role)]
+        (render (assoc request :roles roles))))))
+
+(defn create
+  [{{email :email
+     password :password
+     first :first
+     last :last
+     role-id :role} :params
+     :as request}]
+  (rights/with-permissions "settings/account/create" request
+    (fn [permissions request]
+      (let [account (rights/create permissions
+                                   :account {:email email
+                                             :first-name first
+                                             :last-name last
+                                             :role-id role-id
+                                             :password password})
+            target (route-for :admin.new-account
+                              (select-keys request [:site :locale]))
+            user (dissoc account :created-at :updated-at)]
+        (redirect target {:session (:session request) :user user})))))
+
+;; allow target
+(defn logout
+  [request]
+  (render request {:session (dissoc (:session request) :admin)}))
+
+(defn forgot-password
+  [request]
+  (let [email (-> request :params :email)]
+    ;; send a message to that email that lets them reset their pw
+    (render request)))
+
+(comment
+  ;; here is a way to test out the create-login controller from the repl
+  (caribou.model/db #(caribou.admin.controllers.login/create-login
+                      {:params {:email "justin@weareinstrument.com"
+                                :password "419truth"
+                                :first "Justin"
+                                :last "Lewis"}
+                       :template (constantly "")})))
+
+(comment
+  (caribou.model/db #(caribou.admin.controllers.login/submit-login
+                      {:params {:email "justin@weareinstrument.com"
+                                :password "419truth"
+                                :locale "en_US"}
+                       :template (constantly "")})))
+
+
+(comment
+  (caribou.model/db #(caribou.admin.controllers.login/create-login
+                      {:params {:email "phong@weareinstrument.com"
+                                :password "3Ge5pm!N"
+                                :first "Phong"
+                                :last "Ho"}
+                       :template (constantly "")})))
+
--- src/caribou/admin/routes.clj
+++ src/caribou/admin/routes.clj
@@ -1,51 +1,140 @@
 (ns caribou.admin.routes)
 
 (def admin-routes
-  '({:path "activity", :children (), :slug "admin.activity", :position nil, :template "", :method "", :action "index", :name "Activity", :controller "activity"}
-    {:path "login", :children (), :slug "admin.login", :position nil, :template "login.html", :method "", :action "login", :name "Login", :controller "login"}
-    {:path "logout", :children (), :slug "admin.logout", :position nil, :template "logout.html", :method "", :action "logout", :name "Logout", :controller "login"}
-    {:path "submit-login", :children (), :slug "admin.submit-login", :position nil, :template "", :method "POST", :action "submit-login", :name "Submit Login", :controller "login"}
-    {:path ":site", :slug "admin.subsite", :position nil, :template "", :method "", :action "nothing", :name "Subsite", :controller "login", :children
-     ({:path ":locale", :children
-       ({:path "dashboard", :children (), :slug "admin.dashboard", :position nil, :template "dashboard/index.html", :method "", :action "index", :name "Dashboard", :controller "dashboard"}
-        {:path "content", :children
-         ({:path "projects", :children
-           ({:path "new", :children (), :slug "admin.new-project", :position nil, :template "content/projects/new.html", :method "", :action "new", :name "New Project", :controller "content.projects"}
-            {:path ":id", :children
-             ({:path "edit", :children (), :slug "admin.edit-project", :position nil, :template "content/projects/edit.html", :method "", :action "edit", :name "Edit Project", :controller "content.projects"}),
-             :slug "admin.view-project", :position nil, :template "content/projects/view.html", :method "", :action "view", :name "View Project", :controller "content.projects"}),
-           :slug "admin.projects", :position nil, :template "content/projects/index.html", :method "", :action "index", :name "Projects", :controller "content.projects"}
-          {:path "models", :children
-           ({:path ":slug", :children (), :slug "admin.view-model", :position nil, :template "content/models/view.html", :method "", :action "view", :name "View Model", :controller "content.models"}
-            {:path ":slug/edit", :children (), :slug "admin.edit-model", :position nil, :template "content/models/edit.html", :method "", :action "edit", :name "Edit Model", :controller "content.models"}
-            {:path ":slug/new-field", :children (), :slug "admin.new-model-field", :position nil, :template "", :method "POST", :action "new-field", :name "New Model Field", :controller "content.models"}
-            {:path ":slug/delete-field", :children (), :slug "admin.delete-model-field", :position nil, :template "", :method "POST", :action "delete-field", :name "Delete Model Field", :controller "content.models"}
-            {:path ":slug/results", :children (), :slug "admin.results", :position nil, :template "content/models/instance/results.html", :method "GET", :action "view-results", :name "Results", :controller "content.models"}
-            {:path ":slug/create", :children (), :slug "admin.create-model-instance-post", :position nil, :template "content/models/instance/edit.html", :method "POST", :action "create-instance-post", :name "Create Model Instance Post", :controller "content.models"}
-            {:path ":slug/create", :children (), :slug "admin.create-model-instance", :position nil, :template "content/models/instance/edit.html", :method "GET", :action "create-instance", :name "Create Model Instance", :controller "content.models"}
-            {:path ":slug/:id/edit", :children (), :slug "admin.edit-model-instance-post", :position nil, :template "content/models/instance/edit.html", :method "POST", :action "edit-instance-post", :name "Edit Model Instance Post", :controller "content.models"}
-            {:path ":slug/:id/edit", :children (), :slug "admin.edit-model-instance", :position nil, :template "content/models/instance/edit.html", :method "GET", :action "edit-instance", :name "Edit Model Instance", :controller "content.models"}),
-           :slug "admin.models", :position nil, :template "content/models/index.html", :method "", :action "index", :name "Models", :controller "content.models"}
-          {:path "pages", :children
-           ({:path "new", :children (), :slug "admin.new-page", :position nil, :template "content/pages/new.html", :method "", :action "new", :name "New Page", :controller "content.pages"}
-            {:path ":id", :children
-             ({:path "edit", :children (), :slug "admin.edit-page", :position nil, :template "content/pages/edit.html", :method "", :action "edit", :name "Edit Page", :controller "content.pages"}),
-             :slug "admin.view-page", :position nil, :template "content/pages/view.html", :method "", :action "view", :name "View Page", :controller "content.pages"}),
-           :slug "admin.pages", :position nil, :template "content/pages/index.html", :method "", :action "index", :name "Pages", :controller "content.pages"}
-          {:path "model-api", :children (), :slug "admin.model-api", :position nil, :template "content/models/api.html", :method "", :action "api", :name "Model API", :controller "content.models"}
-          {:path "new-model", :children (), :slug "admin.new-model", :position nil, :template "content/model/new.html", :method "", :action "new", :name "New Model", :controller "content.models"}),
-         :slug "admin.content", :position nil, :template "", :method "", :action "index", :name "Content", :controller "content"}
-        {:path "settings", :children
-         ({:path "models", :children (), :slug "admin.model-list", :position nil, :template "settings/model/index.html", :method "", :action "index", :name "Model List", :controller "settings.model"}
-          {:path "role-editor/:title", :children (), :slug "admin.edit-roles", :position nil, :template "settings/role_editor.html", :method "", :action "edit-roles", :name "Edit Roles", :controller "roles"}
-          {:path "submit-edit-roles/:title", :children (), :slug "admin.submit-edit-roles", :position nil, :template "", :method "POST", :action "submit-edit-roles", :name "Submit Edit Roles", :controller "roles"}
-          {:path "accounts", :children
-           ({:path "new", :children (), :slug "admin.new-account", :position nil, :template "settings/account/new.html", :method "", :action "new", :name "New Account", :controller "settings.account"}
-            {:path "create", :children (), :slug "admin.create-account", :position nil, :template "", :method "POST", :action "create-login", :name "Create Account", :controller "login"}
-            {:path ":id", :children
-             ({:path "edit", :children (), :slug "admin.edit-account", :position nil, :template "settings/account/edit.html", :method "", :action "edit", :name "Edit Account", :controller "settings.account"}),
-             :slug "admin.view-account", :position nil, :template "settings/account/view.html", :method "", :action "view", :name "View Account", :controller "settings.account"}),
-           :slug "admin.accounts", :position nil, :template "settings/account/index.html", :method "", :action "index", :name "Accounts", :controller "settings.account"}),
-         :slug "admin.settings", :position nil, :template "settings.html", :method "", :action "index", :name "Settings", :controller "settings.model"}),
-       :slug "admin.locale", :position nil, :template "", :method "", :action "nothing", :name "Locale", :controller "login"})}
-    {:path "forgot-password", :children (), :slug "admin.forgot-password", :position nil, :template "forgot-password.html", :method "", :action "forgot-password", :name "Forgot Password", :controller "login"}))
+  [{:path "forgot-password", :children [], :slug "admin.forgot-password",
+    :position nil, :template "forgot-password.html", :method "",
+    :action "forgot-password", :name "Forgot Password",
+    :controller "settings.account"}
+   {:path "activity", :children [], :slug "admin.activity", :position nil,
+    :template "", :method "", :action "index", :name "Activity",
+    :controller "activity"}
+   {:path "login", :children [], :slug "admin.login", :position nil,
+    :template "login.html", :method "", :action "login", :name "Login",
+    :controller "settings.account"}
+   {:path "logout", :children [], :slug "admin.logout", :position nil,
+    :template "logout.html", :method "", :action "logout", :name "Logout",
+    :controller "settings.account"}
+   {:path "submit-login", :children [], :slug "admin.submit-login",
+    :position nil, :template "", :method "POST", :action "submit-login",
+    :name "Submit Login", :controller "settings.account"}
+   {:path ":site", :slug "admin.subsite", :position nil, :template "",
+    :method "", :action "nothing", :name "Subsite", :controller "admin.settings",
+    :children
+    [{:path ":locale", :slug "admin.locale", :position nil, :template "",
+      :method "", :action "nothing", :name "Locale",
+      :controller "settings.account"
+      :children
+      [{:path "dashboard", :children [], :slug "admin.dashboard", :position nil,
+        :template "dashboard/index.html", :method "", :action "index",
+        :name "Dashboard", :controller "dashboard"}
+       {:path "content", :slug "admin.content", :position nil, :template "",
+        :method "", :action "index", :name "Content", :controller "content"
+        :children
+        [{:path "projects", :slug "admin.projects", :position nil,
+          :template "content/projects/index.html", :method "", :action "index",
+          :name "Projects", :controller "content.projects"
+          :children
+          [{:path "new", :children [], :slug "admin.new-project", :position nil,
+            :template "content/projects/new.html", :method "", :action "new",
+            :name "New Project", :controller "content.projects"}
+           {:path ":id", :slug "admin.view-project", :position nil,
+            :template "content/projects/view.html", :method "", :action "view",
+            :name "View Project", :controller "content.projects"},
+           :children
+            [{:path "edit", :children [], :slug "admin.edit-project",
+              :position nil, :template "content/projects/edit.html",
+              :method "", :action "edit", :name "Edit Project",
+              :controller "content.projects"}]]}
+         {:path "models", :slug "admin.models", :position nil,
+          :template "content/models/index.html", :method "", :action "index",
+          :name "Models", :controller "content.models",
+          :children
+          [{:path ":slug", :children [], :slug "admin.view-model",
+            :position nil, :template "content/models/view.html", :method "",
+            :action "view", :name "View Model", :controller "content.models"}
+           {:path ":slug/edit", :children [], :slug "admin.edit-model",
+            :position nil, :template "content/models/edit.html", :method "",
+            :action "edit", :name "Edit Model", :controller "content.models"}
+           {:path ":slug/new-field", :children [], :slug "admin.new-model-field",
+            :position nil, :template "", :method "POST", :action "new-field",
+            :name "New Model Field", :controller "content.models"}
+           {:path ":slug/delete-field", :children [],
+            :slug "admin.delete-model-field", :position nil, :template "",
+            :method "POST", :action "delete-field", :name "Delete Model Field",
+            :controller "content.models"}
+           {:path ":slug/results", :children [], :slug "admin.results",
+            :position nil, :template "content/models/instance/results.html",
+            :method "GET", :action "view-results", :name "Results",
+            :controller "content.models"}
+           {:path ":slug/create", :children [],
+            :slug "admin.create-model-instance-post", :position nil,
+            :template "content/models/instance/edit.html", :method "POST",
+            :action "create-instance-post", :name "Create Model Instance Post",
+            :controller "content.models"}
+           {:path ":slug/create", :children [],
+            :slug "admin.create-model-instance", :position nil,
+            :template "content/models/instance/edit.html", :method "GET",
+            :action "create-instance", :name "Create Model Instance",
+            :controller "content.models"}
+           {:path ":slug/:id/edit", :children [],
+            :slug "admin.edit-model-instance-post", :position nil,
+            :template "content/models/instance/edit.html", :method "POST",
+            :action "edit-instance-post", :name "Edit Model Instance Post",
+            :controller "content.models"}
+           {:path ":slug/:id/edit", :children [],
+            :slug "admin.edit-model-instance", :position nil,
+            :template "content/models/instance/edit.html", :method "GET",
+            :action "edit-instance", :name "Edit Model Instance",
+            :controller "content.models"}]}
+         {:path "pages", :slug "admin.pages", :position nil,
+          :template "content/pages/index.html", :method "", :action "index",
+          :name "Pages", :controller "content.pages"
+          :children
+          [{:path "new", :children [], :slug "admin.new-page", :position nil,
+            :template "content/pages/new.html", :method "", :action "new",
+            :name "New Page", :controller "content.pages"}
+           {:path ":id", :slug "admin.view-page", :position nil,
+            :template "content/pages/view.html", :method "", :action "view",
+            :name "View Page", :controller "content.pages"
+            :children
+            [{:path "edit", :children [], :slug "admin.edit-page",
+              :position nil, :template "content/pages/edit.html", :method "",
+              :action "edit", :name "Edit Page", :controller "content.pages"}]}]}
+         {:path "model-api", :children [], :slug "admin.model-api", :position nil
+          :template "content/models/api.html", :method "", :action "api",
+          :name "Model API", :controller "content.models"}
+         {:path "new-model", :children [], :slug "admin.new-model", :position nil
+          :template "content/model/new.html", :method "", :action "new",
+          :name "New Model", :controller "content.models"}]}
+       {:path "settings", :slug "admin.settings", :position nil,
+        :template "settings.html", :method "", :action "index", :name
+        "Settings", :controller "settings.model"
+        :children
+        [{:path "models", :children [], :slug "admin.model-list", :position nil,
+          :template "settings/model/index.html", :method "", :action "index",
+          :name "Model List", :controller "settings.model"}
+         {:path "role-editor/:title", :children [], :slug "admin.edit-roles",
+          :position nil, :template "settings/role_editor.html", :method "",
+          :action "edit-roles", :name "Edit Roles", :controller "roles"}
+         {:path "submit-edit-roles/:title", :children [],
+          :slug "admin.submit-edit-roles", :position nil, :template "",
+          :method "POST", :action "submit-edit-roles", :name "Submit Edit Roles",
+          :controller "roles"}
+         {:path "accounts", :slug "admin.accounts", :position nil,
+          :template "settings/account/index.html", :method "",
+          :action "index", :name "Accounts", :controller "settings.account"
+          :children
+          [{:path "new", :children [], :slug "admin.new-account", :position nil,
+            :template "settings/account/new.html", :method "", :action "new",
+            :name "New Account", :controller "settings.account"}
+           {:path "create", :children [], :slug "admin.create-account",
+            :position nil, :template "", :method "POST", :action "create",
+            :name "Create Account", :controller "settings.account"}
+           {:path ":id", :slug "admin.view-account", :position nil,
+            :template "settings/account/view.html", :method "", :action "view",
+            :name "View Account", :controller "settings.account",
+            :children
+            [{:path "edit", :children [], :slug "admin.edit-account",
+              :position nil, :template "settings/account/edit.html", :method "",
+              :action "edit", :name "Edit Account",
+              :controller "settings.account"}]}]}]}]}]}])
