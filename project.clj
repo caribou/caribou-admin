033c47dca63655dea385107fb4dabe29df2bc359
Justin Glenn Smith <noisesmith@gmail.com>
2013-06-18 16:42:48 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject antler/caribou-admin "0.11.23"
+(defproject antler/caribou-admin "0.11.24"
   :description "Generic admin tool for Caribou projects"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [antler/caribou-frontend "0.11.17"]
+                 [antler/caribou-frontend "0.11.18"]
                  [antler/lichen "0.3.3"]
                  [clj-time "0.4.4"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"
