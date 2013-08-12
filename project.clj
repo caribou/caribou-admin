8b3bb5936eda01f29a32a55d6afeebe4abf79e87
Justin Smith <noisesmith@gmail.com>
2013-08-12 11:47:06 -0700

    version bump for deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-admin "0.11.32"
+(defproject antler/caribou-admin "0.11.33"
   :description "Generic admin tool for Caribou projects"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [antler/caribou-frontend "0.11.26"]
