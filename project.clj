e03213cf777d0dbff00089ae99c6fd4927a064aa
ryan spangler <ryan.spangler@gmail.com>
2014-01-13 14:29:26 -0800

    timepicker version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caribou/caribou-admin "0.13.5"
+(defproject caribou/caribou-admin "0.13.6"
   :description "Generic admin tool for Caribou projects"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [caribou/caribou-frontend "0.13.4"]
