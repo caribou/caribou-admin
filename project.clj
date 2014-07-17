fd5771264db3cd17fbdd4a438b60b0cf482ac678
Justin Glenn Smith <noisesmith@gmail.com>
2014-07-17 10:32:18 -0700

    making the project runnable

--- project.clj
+++ project.clj
@@ -8,6 +8,10 @@
              "-Xmx2g"]
   :source-paths ["src" "../src"]
   :resource-paths ["resources/" "../resources/"]
+  :plugins [[lein-ring "0.8.6"]
+            [caribou/lein-caribou "2.14.0"]
+            [lein-cljsbuild "1.0.2"
+             :exclusions [fs]]]
   :ring {:handler caribou.admin.core/handler
          :servlet-name "caribou-admin"
          :init caribou.admin.core/init
